3e25ed2e621e1b80dd7ed82dd2c475ea4c5b7907
darkasblack <remarkcordova17@gmail.com>
2025-03-01 22:31:53 +0000

    Update2

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import { TempoDevtools } from "tempo-devtools";
 TempoDevtools.init();
 
-const basename = import.meta.env.BASE_URL;
+const basename = import.meta.env.BASE_URL || "/";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter basename={basename}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
