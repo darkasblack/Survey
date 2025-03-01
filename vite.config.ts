aafc770731c4ef9f5bf668851fc4e402cd0add2d
darkasblack <remarkcordova17@gmail.com>
2025-03-01 21:29:28 +0000

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,8 @@ if (process.env.TEMPO === "true") {
   conditionalPlugins.push(["tempo-devtools/swc", {}]);
 }
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE_PATH || "/",
+  base: process.env.NODE_ENV === "development" ? "/" : "/Survey/",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
@@ -29,7 +28,6 @@ export default defineConfig({
     },
   },
   server: {
-    // @ts-ignore
     allowedHosts: true,
-  }
+  },
 });
